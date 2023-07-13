8d56867d30e9c97168fe92439ae79628b6f35b1c
Astar <bahouidi99@gmail.com>
2023-07-13 11:13:09 +0100

    Component update in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [isPDisplayed, setIsPDisplayed] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const togglePDisplay = () => {
+    setIsPDisplayed((prevValue) => !prevValue);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {isPDisplayed && <p>This is new!</p>}
+      <Button onClick={togglePDisplay}>Toggle Paragraph</Button>
     </div>
   );
 }
